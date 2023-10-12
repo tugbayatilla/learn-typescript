51c667c2c1097e4cd634e2c6aaf651d35bfbcf1d
Tugbay Atilla <tatilla@igus.net>
2023-10-12 13:19:06 +0200

    learn: any type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 // Build-in types
 
 let sales: number = 123_456_789;
-let course: string = 'typescript'
+let course: string = "typescript";
 let is_published: boolean = true;
+
+// any type
+let level;
+level = 1;
+level = "a";
+
+function render(document: any) {
+  console.log(document);
+}
 
