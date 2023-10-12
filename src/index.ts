9610d0837294fbbf87b5f537412ff360ace8cc1a
Tugbay Atilla <tatilla@igus.net>
2023-10-12 13:33:45 +0200

    learn: enums

--- src/index.ts
+++ src/index.ts
@@ -32,3 +32,19 @@ console.log(user[0]) // 1
 console.log(user[1]) // 'tugbay'
 
 let bigUser: [number, string, boolean, number] = [1, 'tugbay', true, 42] // not a good idea, because it is unclear
+
+// Enums
+
+const small = 1;
+const medium = 2;
+const large = 3;
+
+// PascalCase
+enum Size { Small, Medium, Large }
+let mySize: Size = Size.Medium;
+console.log('My Size:' + mySize)
+
+const enum Size2 { Small='s', Medium='m', Large='l' } // const makes this code more optimized
+let mySize2: Size2 = Size2.Medium;
+console.log('My Size2:' + mySize2)
+
