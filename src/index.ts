d200dd88f422fb4383ccbc2fb4f666777b8b36a0
Tugbay Atilla <tatilla@igus.net>
2023-10-12 13:54:47 +0200

    learn: objects

--- src/index.ts
+++ src/index.ts
@@ -86,3 +86,41 @@ function calculateTax3(income: number, taxYear: number = 2022): number{
         return income;
 }
 calculateTax3(10_000);
+
+
+// Objects
+
+let employee1 = { id: 1 }
+// employee1.name = 'tugbay' <-- this is not valid, name does not exist
+
+let employee2: { id: number, name: string } // defining the object shape 
+= { id: 1, name: 'tugbay' }
+employee2.name = 'altered name here'
+
+// make optional property
+let employee3: { id: number, name?: string } 
+= { id: 1 }
+employee3.name = 'tugbay'
+
+
+// readonly property
+let employee4: { readonly id: number, name: string } 
+= { id: 1, name: 'tugbay' }
+// employee4.id = 2 <-- this is not valid, id is readonly
+
+
+// Has retire function
+let employee5: { 
+    readonly id: number, 
+    name: string, 
+    retire: (date: Date) => void } 
+= { 
+    id: 1, 
+    name: 'tugbay',
+    retire: (date: Date) => {
+        console.log("Retirement data is " + date)
+    }
+}
+let now = new Date();
+employee5.retire(now)
+
