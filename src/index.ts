627c1d0c8be9ee4e50f9f17623fe0a537e6b74f6
Tugbay Atilla <tatilla@igus.net>
2023-10-12 13:44:37 +0200

    learn: functions

--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,41 @@ const enum Size2 { Small='s', Medium='m', Large='l' } // const makes this code m
 let mySize2: Size2 = Size2.Medium;
 console.log('My Size2:' + mySize2)
 
+// Functions
+
+// always anotate return type
+function calculateTax(income: number): number{
+    // let x = 1; there is an unused variable here. to prevent this, use 'noUnusedLocals: true'
+    if(income < 50_000)
+        return income * 2.2;
+    else
+        return income;
+}
+// javascript return as default 'undefined' in functions. To prevent this, use 'noImplicitReturns: true'
+
+function calculateTax1(income: number, taxYear: number): number{
+    if(taxYear < 2022) 
+        return income * 2.2;
+    else
+        return income;
+}
+calculateTax1(10_000, 2021);
+
+
+// Optional argument
+function calculateTax2(income: number, taxYear?: number): number{
+    if((taxYear || 2022) < 2022) // javascript trick, not a good idea
+        return income * 2.2;
+    else
+        return income;
+}
+calculateTax2(10_000);
+
+// default value
+function calculateTax3(income: number, taxYear: number = 2022): number{
+    if(taxYear < 2022) 
+        return income * 2.2;
+    else
+        return income;
+}
+calculateTax3(10_000);
