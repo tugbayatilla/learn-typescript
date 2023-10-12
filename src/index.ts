b03fbbf97018df6f73c7b747469a75a27b2e1851
Tugbay Atilla <tatilla@igus.net>
2023-10-12 13:26:45 +0200

    learn: tuples

--- src/index.ts
+++ src/index.ts
@@ -25,3 +25,10 @@ anyNumbers[1] = '1' // this is valid because anyNumbers type is any[]
 
 let emptyNumberArray: number[] = [] // ampty array
 
+// Tuples
+
+let user: [number, string] = [1, 'tugbay'] // exactly 2 elements
+console.log(user[0]) // 1
+console.log(user[1]) // 'tugbay'
+
+let bigUser: [number, string, boolean, number] = [1, 'tugbay', true, 42] // not a good idea, because it is unclear
