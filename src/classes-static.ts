dcff19a6f453a5479a583c5d08385e3bf2bc0235
Tugbay Atilla <tugbayatilla@gmail.com>
2023-10-18 13:43:28 +0200

    learn: class static members

--- src/classes-static.ts
+++ src/classes-static.ts
@@ -1,8 +1,12 @@
 class Ride {
-    activeRides: number = 0;
+    private static _activeRides: number = 0;
     
-    start(): void { this.activeRides++;}
-    stop(): void { this.activeRides--; }
+    start(): void { Ride._activeRides++;}
+    stop(): void { Ride._activeRides--; }
+    
+    static get activeRides(){
+        return Ride._activeRides;
+    }
 }
 
 let uber1 = new Ride();
@@ -11,5 +15,6 @@ uber1.start()
 let uber2 = new Ride();
 uber2.start();
 
-console.log(uber1.activeRides) // returns 1
-console.log(uber2.activeRides) // returns 2
+// Ride.activeRides = 100; this cannot be assigned because it has only setter.
+
+console.log(Ride.activeRides) // returns 2
