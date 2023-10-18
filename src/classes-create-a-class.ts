b5d817b0c79fff596fbad1d0ff1e945c9b9a067a
Tugbay Atilla <tugbayatilla@gmail.com>
2023-10-18 13:19:38 +0200

    learn: class access control keywords

--- src/classes-create-a-class.ts
+++ src/classes-create-a-class.ts
@@ -1,20 +1,31 @@
 class Account{
     readonly id: number; // these properties are not exist in javascript, only exist in Typescript
     owner: string; // these are public properties
-    balance: number;
+    private _balance: number;
     nickname?: string // this property is created later but as optional 
     
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     
     deposit(amount: number):void {
         // this.id = 0; this should not happen. useing readonly keyword
         if(amount<0)
             throw new Error('Invalid amount');
-        this.balance += amount;
+        
+        // record transaction object
+        
+        this._balance += amount + this.calculateTax();
+    }
+    
+    getBalance(): number{ // this is an option you can make but there is a better solution
+        return this._balance;
+    }
+    
+    private calculateTax():number { // this is another private method 
+        return 0;
     }
 }
 
@@ -25,3 +36,5 @@ console.log(account)
 // Union
 console.log(typeof account) // returns object
 console.log(account instanceof Account) // returns true or false
+
+console.log(account.getBalance()) // there is better solution than this one.
