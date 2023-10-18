eb7e8e8b11b8ff2cff6baf01de3625820e35853d
Tugbay Atilla <tugbayatilla@gmail.com>
2023-10-18 13:42:29 +0200

    fix: tsc build

--- src/advanced-unknown-type.ts
+++ src/advanced-unknown-type.ts
@@ -6,11 +6,13 @@ function render1(document: any){ // defining any is not a good idea
     document.whateverWeWant()
 } 
 
+/*
 function render2(document: unknown){ // defining unknown instead of any is a good idea
     //document.move() <-- compiler error
     //document.fly() <-- compiler error
     //document.whateverWeWant() <-- compiler error
 } 
+*/
 
 function render3(document: unknown){ 
     // Narrowing
